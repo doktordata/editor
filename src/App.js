af7253109121f519687a6b0385e5857aa3be9fba
Erik Palm <erik.palm@ica.se>
2019-05-17 16:28:36 +0200

    Slate init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import styled, { createGlobalStyle } from "styled-components"
+import { Editor } from "slate-react"
+import { Value } from "slate"
+import initialValue from "./model/value.json"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const Container = styled.div`
+  padding: 24px;
+`
+
+const StyledEditor = styled(Editor)`
+  width: 100%;
+  height: 100vh;
+`
+
+const CodeNode = props => (
+  <pre {...props.attributes}>
+    <code>{props.children}</code>
+  </pre>
+)
+const ParagraphNode = props => <p {...props.attributes}>{props.children}</p>
+const BoldMark = props => <strong>{props.children}</strong>
 
 function App() {
+  const [value, setValue] = useState(Value.fromJSON(initialValue))
+
+  const onChange = ({ value }) => {
+    setValue(value)
+  }
+
+  const onKeyDown = (event, editor, next) => {
+    if (!event.ctrlKey) return next()
+
+    switch (event.key) {
+      case "b": {
+        event.preventDefault()
+        editor.toggleMark("bold")
+        break
+      }
+      case "'": {
+        const isCode = editor.value.blocks.some(block => block.type === "code")
+        event.preventDefault()
+        editor.setBlocks(isCode ? "paragraph" : "code")
+        break
+      }
+      default: {
+        return next()
+      }
+    }
+  }
+
+  const renderBlock = (props, editor, next) => {
+    switch (props.node.type) {
+      case "code":
+        return <CodeNode {...props} />
+      case "paragraph":
+        return <ParagraphNode {...props} />
+      default:
+        return next()
+    }
+  }
+
+  const renderMark = (props, editor, next) => {
+    switch (props.mark.type) {
+      case "bold":
+        return <BoldMark {...props} />
+      default:
+        return next()
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+      <Container>
+        <StyledEditor
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          renderBlock={renderBlock}
+          renderMark={renderMark}
+          autoFocus
+        />
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
